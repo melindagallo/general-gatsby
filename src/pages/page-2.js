1d23a7bb21628599290c31b1ae03098f3493515d
melindagallo <68917193+melindagallo@users.noreply.github.com>
2021-05-13 11:40:37 +0200

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -4,30 +4,12 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
-
-/*export default function App() {
-  const [isActive, setActive] = useState("false");
-
-  const handleToggle = () => {
-    setActive(!isActive);
-  };
-  return (isActive);
-} */
-
 const SecondPage = () => (
   <Layout>
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
-  
-    <div className="base">
-    <button type="button" onClick={this.handleToggle} 
-    className={this.isActive ? "active" : ""}>
-      <span class="plus">+</span>
-      <span class="minus">-</span>
-    </button>
-  </div>
   </Layout>
 )
 
